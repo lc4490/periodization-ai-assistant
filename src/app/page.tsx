f90eea34e014515c9d2f0dfd505adfa0245592f0
Leo <lc4490@nyu.edu>
2025-06-21 08:46:16 -0400

    multi-lang

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,96 @@
 "use client";
-import { Box, Button, Stack, TextField, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  createTheme,
+  CssBaseline,
+  NativeSelect,
+  Stack,
+  TextField,
+  ThemeProvider,
+  Typography,
+  useMediaQuery,
+} from "@mui/material";
 import { useState } from "react";
 import PersonIcon from "@mui/icons-material/Person";
 import AssistantIcon from "@mui/icons-material/Assistant";
 import { useEffect } from "react";
 
+// light/dark themes
+const lightTheme = createTheme({
+  palette: {
+    mode: "light",
+    background: {
+      default: "#ffffff",
+      paper: "#ffffff",
+    },
+    text: {
+      primary: "#000000",
+    },
+  },
+});
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    background: {
+      default: "#121212",
+      paper: "#121212",
+    },
+    text: {
+      primary: "#ffffff",
+    },
+  },
+});
+
 export default function Home() {
+  // loading state
+  const [isLoading, setIsLoading] = useState(false);
+  // multi-language
+  const [selectedLanguage, setSelectedLanguage] = useState(
+    "en"
+    // navigator.language.split("-")[0]
+  );
+  console.log(selectedLanguage);
+
+  const handleLanguageChange = (event: { target: { value: any } }) => {
+    const newLang = event.target.value;
+    setSelectedLanguage(newLang);
+    setMessages([
+      {
+        role: "assistant",
+        content: getInitialGreeting(newLang),
+      },
+    ]);
+  };
+
+  const getInitialGreeting = (lang = "en") => {
+    switch (lang) {
+      case "zh":
+        return "👋 你好！我是你的多语言训练助手。欢迎用中文提问关于力量训练、增肌、周期训练等问题！";
+      case "es":
+        return "👋 ¡Hola! Soy tu asistente multilingüe de entrenamiento. Pregúntame sobre entrenamiento de fuerza, hipertrofia, periodización, o rendimiento atlético.";
+      case "fr":
+        return "👋 Salut ! Je suis ton assistant d'entraînement multilingue. Pose-moi des questions sur l'entraînement de force, l'hypertrophie, la périodisation ou la performance.";
+      default:
+        return "👋 Hi! I’m your multilingual training assistant. Ask me anything about strength training, hypertrophy, periodization, or athletic performance — in any language. I’ll respond in the same language you use.";
+    }
+  };
+
+  // detect light/dark mode, set light dark mode
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  const [darkMode, setDarkMode] = useState(prefersDarkMode);
+  useEffect(() => {
+    setDarkMode(prefersDarkMode);
+  }, [prefersDarkMode]);
+  const theme = darkMode ? darkTheme : lightTheme;
+
   const [messages, setMessages] = useState([
     {
       role: "assistant",
-      content:
-        "👋 Hi! I’m your multilingual training assistant. Ask me anything about strength training, hypertrophy, periodization, or athletic performance — in any language. I’ll respond in the same language you use.",
+      content: getInitialGreeting(),
     },
   ]);
   const [message, setMessage] = useState("");
-  // loading state
-  const [isLoading, setIsLoading] = useState(false);
   const sendMessage = async () => {
     // when message is sending, set state to loading to prevent overloading
     if (!message.trim() || isLoading) return;
@@ -90,41 +165,41 @@ export default function Home() {
     setMessages([
       {
         role: "assistant",
-        content:
-          "👋 Hi! I’m your multilingual training assistant. Ask me anything about strength training, hypertrophy, periodization, or athletic performance — in any language. I’ll respond in the same language you use.",
+        content: getInitialGreeting(),
       },
     ]);
   };
   return (
-    <Box
-      width="100vw"
-      height="100vh"
-      display="flex"
-      flexDirection="column"
-      justifyContent="center"
-      alignItems="center"
-    >
-      <Stack
-        direction={"column"}
-        width="100%"
-        height="100%"
-        border="1px solid black"
-        p={2}
-        spacing={3}
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Box
+        width="100vw"
+        height="100vh"
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
       >
-        {/* header box */}
-        <Box
-          height="10%"
-          // bgcolor="background.default"
-          display="flex"
-          justifyContent="space-between"
-          paddingX={2.5}
-          paddingY={2.5}
-          alignItems="center"
-          position="relative"
+        <Stack
+          direction={"column"}
+          width="100%"
+          height="100%"
+          p={2}
+          spacing={3}
         >
-          {/* language control  */}
-          {/* <FormControl
+          {/* header box */}
+          <Box
+            height="10%"
+            // bgcolor="background.default"
+            display="flex"
+            justifyContent="space-between"
+            paddingX={2.5}
+            paddingY={2.5}
+            alignItems="center"
+            position="relative"
+          >
+            {/* language control  */}
+            {/* <FormControl
           sx={{
             width: '85px', // Adjust the width value as needed
           }}
@@ -165,17 +240,29 @@ export default function Home() {
               <option value="kr">한국어</option>
             </NativeSelect>
           </FormControl> */}
-          <Box></Box>
-          {/* <Button>Equipment</Button> */}
-          {/* title */}
-          <Box display="flex" flexDirection={"row"} alignItems={"center"}>
-            <Typography variant="h6" textAlign="center">
-              {"AI Trainer"}
-            </Typography>
-          </Box>
-          {/* signIn/signOut Form */}
-          <Box></Box>
-          {/* <Box>
+            <NativeSelect
+              value={selectedLanguage}
+              onChange={handleLanguageChange}
+            >
+              <option value="en">English</option>
+              <option value="zh">中文</option>
+              <option value="es">Español</option>
+              <option value="fr">Français</option>
+              <option value="de">Deutsch</option>
+              <option value="ja">日本語</option>
+              <option value="ko">한국어</option>
+            </NativeSelect>
+
+            {/* <Button>Equipment</Button> */}
+            {/* title */}
+            <Box display="flex" flexDirection={"row"} alignItems={"center"}>
+              <Typography variant="h6" textAlign="center">
+                {"AI Trainer"}
+              </Typography>
+            </Box>
+            {/* signIn/signOut Form */}
+            <Box></Box>
+            {/* <Box>
             {!user ? (
               <Button
                 onClick={handleSignIn}
@@ -213,89 +300,76 @@ export default function Home() {
               </Button>
             )}
           </Box> */}
-        </Box>
-        <Stack
-          direction={"column"}
-          spacing={2}
-          flexGrow={1}
-          overflow="auto"
-          maxHeight="100%"
-          className="chat-log"
-        >
-          {/* message log */}
-          {messages.map((message, index) => (
-            <Box
-              key={index}
-              display="flex"
-              justifyContent={
-                message.role === "assistant" ? "flex-start" : "flex-end"
-              }
-            >
-              {message.role === "assistant" && (
-                <AssistantIcon sx={{ mr: 1, fontSize: "2.5rem" }} />
-              )}
+          </Box>
+          <Stack
+            direction={"column"}
+            spacing={2}
+            flexGrow={1}
+            overflow="auto"
+            maxHeight="100%"
+            className="chat-log"
+          >
+            {/* message log */}
+            {messages.map((message, index) => (
               <Box
-                bgcolor={
-                  message.role === "assistant"
-                    ? "primary.main"
-                    : "secondary.main"
+                key={index}
+                display="flex"
+                justifyContent={
+                  message.role === "assistant" ? "flex-start" : "flex-end"
                 }
-                color="white"
-                borderRadius={16}
-                p={3}
               >
-                {message.content}
+                {message.role === "assistant" && (
+                  <AssistantIcon sx={{ mr: 1, fontSize: "2.5rem" }} />
+                )}
+                <Box
+                  bgcolor={
+                    message.role === "assistant"
+                      ? "primary.main"
+                      : "secondary.main"
+                  }
+                  borderRadius={16}
+                  p={3}
+                >
+                  {message.content}
+                </Box>
+                {message.role === "user" && (
+                  <PersonIcon sx={{ ml: 1, fontSize: "2.5rem" }} />
+                )}
               </Box>
-              {message.role === "user" && (
-                <PersonIcon sx={{ ml: 1, fontSize: "2.5rem" }} />
-              )}
-            </Box>
-          ))}
-        </Stack>
-        {/* input field */}
-        <Stack
-          direction="row"
-          spacing={2}
-          padding={2}
-          sx={{ width: "100%", bottom: 0 }}
-        >
-          <TextField
-            label={"Message"}
-            fullWidth
-            value={message}
-            variant="outlined"
-            onChange={(e) => setMessage(e.target.value)}
-            onKeyDown={handleKeyPress}
-            disabled={isLoading}
-            sx={{ backgroundColor: "white", borderRadius: "8px" }}
-          ></TextField>
-          {/* send button */}
-          <Button
-            variant="outlined"
-            onClick={sendMessage}
-            disabled={message == ""}
-            sx={{
-              backgroundColor: "white",
-              color: "black",
-              borderColor: "black",
-            }}
+            ))}
+          </Stack>
+          {/* input field */}
+          <Stack
+            direction="row"
+            spacing={2}
+            padding={2}
+            sx={{ width: "100%", bottom: 0 }}
           >
-            {"send"}
-          </Button>
-          {/* clear history */}
-          <Button
-            onClick={clearChatLog}
-            variant="outlined"
-            sx={{
-              backgroundColor: "white",
-              color: "black",
-              borderColor: "black",
-            }}
-          >
-            {"clear"}
-          </Button>
+            <TextField
+              label={"Message"}
+              fullWidth
+              value={message}
+              variant="outlined"
+              onChange={(e) => setMessage(e.target.value)}
+              onKeyDown={handleKeyPress}
+              disabled={isLoading}
+              sx={{ borderRadius: "8px" }}
+            ></TextField>
+            {/* send button */}
+            <Button
+              variant="outlined"
+              onClick={sendMessage}
+              disabled={message == ""}
+            >
+              {"send"}
+            </Button>
+            {/* clear history */}
+            <Button onClick={clearChatLog} variant="outlined">
+              {"clear"}
+            </Button>
+          </Stack>
         </Stack>
-      </Stack>
-    </Box>
+      </Box>
+    </ThemeProvider>
   );
 }
